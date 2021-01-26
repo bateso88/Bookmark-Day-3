1047281fcf7f1f4666c36a5d6572450bb3ed3b80
bateso88 <bateso@hotmail.co.uk>
2021-01-26 17:29:37 +0000

    Stage 11 complete

--- spec/features/add_new_bookmark_spec.rb
+++ spec/features/add_new_bookmark_spec.rb
@@ -2,7 +2,8 @@ feature 'adding a bookmark' do
     scenario ' I would like to add new bookmarks' do 
         visit('/bookmarks/new')
         fill_in :url, with: 'www.hello.com'
+        fill_in :title, with: 'Test Bookmark'
         click_button 'Submit'
-        expect(page).to have_content 'www.hello.com'
+        expect(page).to have_link('Test Bookmark', href: 'www.hello.com')
     end 
 end 
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -6,14 +6,14 @@ feature 'Viewing bookmarks' do
 
 	scenario 'A user can see bookmarks' do
    
-   Bookmark.create(url: "http://www.makersacademy.com")
-   Bookmark.create(url: "http://www.destroyallsoftware.com")
-   Bookmark.create(url: "http://www.google.com")
+   Bookmark.create(url: "http://www.makersacademy.com", title: "Makers")
+   Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy all software")
+   Bookmark.create(url: "http://www.google.com", title: "Google")
    visit('/bookmarks')
 
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_link('Makers', href: "http://www.makersacademy.com")
+    expect(page).to have_link('Destroy all software', href: "http://www.destroyallsoftware.com")
+    expect(page).to have_link('Google', href: "http://www.google.com")
   end
 
 end 
